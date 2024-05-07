5dd3ba6338a01816f4924fa276f8d63fd36138a5
PerryPhan <daiphan308@gmail.com>
2024-05-07 17:12:26 +0700

    [master] Init the extension to show on browser with single click

--- background.js
+++ background.js
@@ -3,12 +3,13 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
         // When a page contains a <video> tag...
-        new chrome.declarativeContent.PageStateMatcher({
+        /* new chrome.declarativeContent.PageStateMatcher({
           pageUrl: { schemes: ['https', 'http'] }
         })
+        */
       ],
       // ... show the page action.
-      actions: [new chrome.declarativeContent.ShowPageAction() ]
+      // actions: [new chrome.declarativeContent.ShowPageAction() ]
     }]);
   });
 });
